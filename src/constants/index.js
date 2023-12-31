df51dd60fa17411645a0c0bba93df977bb658ac2
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-12-31 23:38:41 +0530

    updated customer review section

--- src/constants/index.js
+++ src/constants/index.js
@@ -79,9 +79,15 @@ export const reviews = [
     },
     {
         imgURL: customer2,
-        customerName: 'Lota Mongeskar',
+        customerName: 'Lata Mongeskar',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    },
+    {
+        imgURL: customer1,
+        customerName: 'John Smith',
+        rating: 4.6,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     }
 ];
 
